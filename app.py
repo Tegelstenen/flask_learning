96e874c322f61a1a4b7a5b58cb72151280934e74
Filip Sjöstrand <f.w.sjostrand@gmail.com>
2024-04-01 21:05:45 +0200

    Updated requirements

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 from flask import Flask, render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-def load_jobs_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM jobs"))
-        jobs = []
-        for row in result.all():
-            row_dict = row._asdict()
-            jobs.append(row_dict)
-    return jobs
 
 @app.route("/")
 def hello_world():
--- database.py
+++ database.py
@@ -1,16 +1,21 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-
-# Replace the placeholders with your actual Aiven database credentials and paths
 con_string = "mysql+pymysql://avnadmin:AVNS_kPof7KK93MJNIRlbDaH@sensacareers-sensacareers.a.aivencloud.com:28183/defaultdb?charset=utf8mb4"
-
-# Path to the Aiven CA certificate you downloaded
 ca_cert_path = "/Users/filipsjostrand/ca.pem"
 
 engine = create_engine(
     con_string,
     connect_args={"ssl": {"ca": ca_cert_path}},
 )
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
+        jobs = []
+        for row in result.all():
+            row_dict = row._asdict()
+            jobs.append(row_dict)
+    return jobs
 
 
 
